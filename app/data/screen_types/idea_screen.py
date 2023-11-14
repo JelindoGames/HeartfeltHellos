6997d3aaad1143921b675f4e36c452cf8b7365bb
JelindoGames <jmlehman97@earthlink.net>
2023-11-14 17:07:03 -0500

    Working dynamic widgets

--- app/data/screen_types/idea_screen.py
+++ app/data/screen_types/idea_screen.py
@@ -4,9 +4,13 @@ from app.widgets.heartfelt_hellos_idea_button import HeartfeltHellosIdeaButton
 
 class IdeaScreen(ShowcaseScreen):
 
+    dynamic_widget = None
+
     def on_pre_enter(self, *args):
         print("PreEnter")
-        new_dynamic_widget = HeartfeltHellosIdeaButton()
-        new_dynamic_widget.text = "Dynamic Button"
-        self.ids.Test.add_widget(new_dynamic_widget)
-        #self.ids.Test.remove_widget(new_dynamic_widget)
+        self.dynamic_widget = HeartfeltHellosIdeaButton()
+        self.dynamic_widget.text = "Dynamic Button"
+        self.ids.Test.add_widget(self.dynamic_widget)
+
+    def on_leave(self, *args):
+        self.ids.Test.remove_widget(self.dynamic_widget)
