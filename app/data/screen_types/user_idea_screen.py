ba9c6f108a099c95aef84b81676ab8c5468496a8
JelindoGames <jmlehman97@earthlink.net>
2023-11-20 10:59:11 -0800

    Tag carries over to idea selection

--- app/data/screen_types/user_idea_screen.py
+++ app/data/screen_types/user_idea_screen.py
@@ -1,13 +1,25 @@
 from app.data.screen_types.idea_screen import IdeaScreen
 from app.data.data_types.idea import Idea
+from kivy.app import App
 
 
 class UserIdeaScreen(IdeaScreen):
 
     def get_ideas(self):
-        return [Idea("Do you still play soccer?", ["Tag 1", "Tag 2"]),
-                Idea("Did you see the game last Sunday?", ["Tag 3", "Tag 4"]),
-                Idea("What's your favorite team lately?", ["Tag 3", "Tag 4"])]
+        selected_tag = App.get_running_app().stored_data.temp_selected_tag
+        # TODO something much more advanced
+        if selected_tag == "sports":
+            return [Idea("Do you still play soccer?", ["Tag 1", "Tag 2"]),
+                    Idea("Did you see the game last Sunday?", ["Tag 3", "Tag 4"]),
+                    Idea("What's your favorite team lately?", ["Tag 3", "Tag 4"])]
+        elif selected_tag == "movies":
+            return [Idea("Have you seen the FNAF movie?", ["Tag 1", "Tag 2"]),
+                    Idea("What movies are you interested in seeing soon?", ["Tag 1", "Tag 2"])]
+        elif selected_tag == "books":
+            return [Idea("Read any good books lately?", ["Tag 1", "Tag 2"]),
+                    Idea("What's the worst book you've ever read?", ["Tag 1", "Tag 2"])]
+        else:
+            return []
 
     def get_header(self):
         return "User Ideas"
