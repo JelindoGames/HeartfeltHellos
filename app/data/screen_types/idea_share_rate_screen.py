5bce10c47b8ba322af56310955807d2765bef9d8
JelindoGames <jmlehman97@earthlink.net>
2023-11-20 13:05:35 -0800

    Messy transition to message screen

--- app/data/screen_types/idea_share_rate_screen.py
+++ app/data/screen_types/idea_share_rate_screen.py
@@ -20,14 +20,20 @@ class ShareRateScreen(Screen):
         idea_button.do_nothing_on_touch_down()
         self.box_layout.add_widget(idea_button)
         friend = App.get_running_app().stored_data.temp_selected_person
-        self.box_layout.add_widget(HeartfeltHellosButton(text=f"Share with {friend}", size_hint_y=None, height='50dp'))
+        self.box_layout.add_widget(HeartfeltHellosButton(text=f"Share with {friend.name}", size_hint_y=None, height='50dp', on_press=self.on_share_pressed))
         self.box_layout.add_widget(Label(text="Rate this idea", font_size='30dp', color=(0, 0, 0)))
         self.rating_layout = GridLayout(cols=5, size_hint_y=None, height='40dp')
         self.box_layout.add_widget(self.rating_layout)
         for i in range(5):
             self.rating_layout.add_widget(HeartfeltHellosButton(text=str(i+1)))
-        self.box_layout.add_widget(HeartfeltHellosButton(text="View Follow-Up Ideas", size_hint_y=None, height='40dp'))
+        self.box_layout.add_widget(HeartfeltHellosButton(text="View Follow-Up Ideas", size_hint_y=None, height='40dp', on_press=self.on_follow_up_pressed))
         self.add_widget(self.box_layout)
+
+    def on_share_pressed(self, arg):
+        App.get_running_app().go_screen("Message_Screen", "left")
+
+    def on_follow_up_pressed(self, arg):
+        App.get_running_app().go_screen("Sub_Idea_Screen", "left")
 
     def share_idea(self):
         # Logic to share the idea
--- app/data/screen_types/message_screen.py
+++ app/data/screen_types/message_screen.py
@@ -1,7 +1,33 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.app import App
+from app.widgets.new_idea_button import HeartfeltHellosNewIdeaButton
+from app.widgets.heartfelt_hellos_button import HeartfeltHellosButton
+
 
 class MessageScreen(Screen):
-    def send_message(self):
-        # Logic to send the message
-        message = self.ids.message_input.text
-        print(f'Message sent: {message}')
+
+    master_layout = None
+    name_layout = None
+    message_layout = None
+    writing_layout = None
+
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+        self.master_layout = BoxLayout(orientation='vertical')
+        self.name_layout = BoxLayout(size_hint_y=0.1, orientation='horizontal')
+        self.message_layout = BoxLayout(size_hint_y=0.8, orientation='vertical')
+        self.writing_layout = BoxLayout(size_hint_y=0.1, orientation='horizontal')
+        self.add_widget(self.master_layout)
+        self.master_layout.add_widget(self.name_layout)
+        self.master_layout.add_widget(self.message_layout)
+        self.master_layout.add_widget(self.writing_layout)
+        self.name_layout.add_widget(Label(text=App.get_running_app().stored_data.temp_selected_person.name))
+        self.writing_layout.add_widget(TextInput(text=App.get_running_app().stored_data.temp_selected_idea.prompt, size_hint_x=0.8))
+        self.writing_layout.add_widget(HeartfeltHellosButton(text="Send", size_hint_x=0.2))
+
+    def send_message(self, message):
+        print(message)
