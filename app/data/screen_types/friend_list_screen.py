9d7d8fbb2adaf413dfe9ba15249e4ec7bbe3990e
Luna <48360271+yuile525@users.noreply.github.com>
2023-11-19 20:54:46 -0500

    minor helper functions created for screen connecting later on

--- app/data/screen_types/friend_list_screen.py
+++ app/data/screen_types/friend_list_screen.py
@@ -14,6 +14,7 @@ class FriendScreen(ShowcaseScreen):
 
     def __init__(self, **kwargs):
         super(FriendScreen, self).__init__(**kwargs)
+        #self.friends = friends
         self.grid_layout = GridLayout(spacing='10dp', padding='10dp', cols=1, size_hint_y=None)
         self.grid_layout.bind(minimum_height=self.grid_layout.setter("height"))
         self.scroll_view = ScrollView(do_scroll_y=True)
@@ -24,7 +25,7 @@ class FriendScreen(ShowcaseScreen):
         self.grid_layout.clear_widgets()
         
         # add friend button rendering
-        add_friend_widget = HeartfeltHellosAddFriendButton(on_press=lambda x: print("Pressed add friend"))
+        add_friend_widget = HeartfeltHellosAddFriendButton(on_press=lambda x: self.pressed_add_friend())
         self.grid_layout.add_widget(add_friend_widget)
 
         # divider rendering
@@ -32,11 +33,20 @@ class FriendScreen(ShowcaseScreen):
         self.grid_layout.add_widget(line)
 
         for friend in self.get_friends():
-            friend_name_widget = HeartfeltHellosFriendButton(friend, on_press=lambda x: print("Pressed " + friend.name))
+            friend_name_widget = HeartfeltHellosFriendButton(friend, on_press=lambda x: self.pressed_friend(x.text))
             self.grid_layout.add_widget(friend_name_widget)
+
+    def pressed_add_friend(self):
+        # pressed add friend, go to correct screen
+        print("Pressed add friend")
+
+    def pressed_friend(self,name):
+        # pressed friend, go to correct screen
+        print("Pressed " + name)
 
     def on_leave(self, *args):
         self.grid_layout.clear_widgets()
 
     def get_friends(self) -> list:
+        #return self.friends
         return [Friend("Michael", ["Tag 1", "Tag 2", "Tag 3"])]
