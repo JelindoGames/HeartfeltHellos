df0b45d5781bf635e15198ddf4b8177ac7bf08d0
Luis A Garcia <67930113+LugaGarcia@users.noreply.github.com>
2023-11-20 05:16:14 -0500

    Luis Created idea_share_rate_screen.py

---
+++ app/data/screen_types/idea_share_rate_screen.py
@@ -0,0 +1,10 @@
+from kivy.uix.screenmanager import Screen
+
+class ShareRateScreen(Screen):
+    def share_idea(self):
+        # Logic to share the idea
+        pass
+
+    def rate_idea(self, rating):
+        # Logic to rate the idea
+        print(f'Idea rated as: {rating}')
