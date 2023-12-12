577dbb9deb13d05e0ab73e207ba252257320d6a4
Luna <48360271+yuile525@users.noreply.github.com>
2023-12-12 17:08:24 -0500

    sorting user ideas alphabetically

--- app/data/screen_types/user_idea_screen.py
+++ app/data/screen_types/user_idea_screen.py
@@ -13,8 +13,21 @@ class UserIdeaScreen(IdeaScreen):
     def get_ideas_with_tag(self, tag: str):
         ideas_with_tag = []
         for idea in App.get_running_app().stored_data.ideas:
+            isAdded = False
             if (idea.hasTag(tag)) :
-                ideas_with_tag.append(idea)
+                # adding to sort
+                if (len(ideas_with_tag) != 0):
+                    # compare rating and place in the right spot
+                    for tag_idea in ideas_with_tag:
+                        if (tag_idea.get_rating() < idea.get_rating()):
+                            ideas_with_tag.insert(ideas_with_tag.index(tag_idea), idea)
+                            isAdded = True
+                            break
+
+                if (not isAdded):              
+                    ideas_with_tag.append(idea)
+                    isAdded = True
+            
         return ideas_with_tag
 
     def get_header(self):
